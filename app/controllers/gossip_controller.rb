b319f7cd86fdbf4c75acf76779f53ce43f9c36ae
Mathieu <mathieu.joly@lrguas.ch>
2020-08-03 18:52:17 +0200

    third

---
+++ app/controllers/gossip_controller.rb
@@ -0,0 +1,6 @@
+class GossipController < ApplicationController
+  def gossip
+    puts "démarrage"
+   puts "Ceci est mon super id dans params : #{params}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/team', to: 'team#show'
+  get '/contact', to: 'contact#show'
+  get '/welcome/:first_name', to: 'welcome#show'
+  get '/welcome', to: 'welcome#show'
+  get '/:gossip', to: 'gossip#gossip', as:'gossip'
 end
