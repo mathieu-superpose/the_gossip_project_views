8b171335db91f9e86e002d01babeda657c7e27f7
Mathieu <mathieu.joly@lrguas.ch>
2020-08-04 02:01:39 +0200

    up image de profil aléatoire

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,8 @@ class WelcomeController < ApplicationController
      @first_name = params[:first_name]
      puts "Ceci est mon super params : #{params}"
      puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans tel champ : #{params["first_name"]}"
-     @images  = ["https://www.bootdey.com/img/Content/user_1.jpg", "https://www.bootdey.com/img/Content/user_2.jpg", "https://www.bootdey.com/img/Content/user_3.jpg"]
-     @random_no = rand(3)
-     @random_image = @images[@random_no]
+     # @images  = ["https://www.bootdey.com/img/Content/user_1.jpg", "https://www.bootdey.com/img/Content/user_2.jpg", "https://www.bootdey.com/img/Content/user_3.jpg"]
+     # @random_no = rand(3)
+     # @random_image = @images[@random_no]
   end
 end
